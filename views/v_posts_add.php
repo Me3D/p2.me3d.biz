8ad47c03414114bf1cb186e6ad03027431b8ce47
Me3D <markstites@fas.harvard.edu>
2013-10-27 16:24:15 -0500

    Big push. Adds posts, followers and follwing

---
+++ views/v_posts_add.php
@@ -0,0 +1,12 @@
+<div class="container">
+<h1>HEY HEY HEY </h1>
+
+<form method='post' action='/posts/p_add'>
+
+	<textarea name='content'></textarea>
+
+	<input type='submit' value='Add new post'>
+
+</form>
+
+</div>
---
+++ views/v_posts_index.php
@@ -0,0 +1,7 @@
+<br><br>
+
+<?php foreach($posts as $post): ?>
+	<?=$post['first_name']?><br>
+	<?=$post['content']?><br><br>
+
+<?php endforeach; ?>
